bcf7f79b92572d3e5cdaa3a718f98416be251152
isalevine <isa.loves.coding@gmail.com>
2019-09-29 08:36:13 -0700

    added commented-out code for sorting by successful payments--still need to add migration to add status to payments

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,10 @@
 class Order < ApplicationRecord
     has_many :payments
+
+    # has_many :payments do
+    #     def successful
+    #         where("payment.status = ?", "Successful")
+    #     end
+    # end
+    
 end
