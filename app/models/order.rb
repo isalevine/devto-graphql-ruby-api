628f312ac75bd5c753aa5e6901f89dcfe75d3874
isalevine <isa.loves.coding@gmail.com>
2019-09-29 10:24:27 -0700

    added failed seed; reverted order model for dev.to article

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,3 @@
 class Order < ApplicationRecord
     has_many :payments
-
-    # has_many :payments do
-    #     def successful
-    #         where("payment.status = ?", "Successful")
-    #     end
-    # end
-    
 end
