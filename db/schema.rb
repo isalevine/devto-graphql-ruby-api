174ea46513f55ab0fa2258665be9e95431d0909e
isalevine <isa.loves.coding@gmail.com>
2019-09-29 08:43:07 -0700

    migrated and added status to payments and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_153644) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
